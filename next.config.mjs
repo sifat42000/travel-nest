37763e0c11ef9d01bbd3d88bea55e796466f7544
sifat42000 <rashiquzzamansifat@gmail.com>
2025-01-28 03:20:15 +0600

    all finishh

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      fontLoaders: [
-        { loader: '@next/font/google', options: { subsets: ['latin'] } },
-      ],
+    eslint: {
+        ignoreDuringBuilds: true,
     },
-  };
+
+};
 
 export default nextConfig;
